c0947242805f01988e0b4965031d2eba22307745
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-24 17:39:54 -0600

    Create AreaTriangulo.cs

---
+++ TareaEjercicio Abstracción/AreaTriangulo.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace TareaEjercicio_Abstracción
+{
+    internal class AreaTriangulo : FormulaMatematica
+    {
+        private double baseTriangulo;
+        private double alturaTriangulo;
+
+        public AreaTriangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.alturaTriangulo = alturaTriangulo;
+        }
+
+        public override void Calcular()
+        {
+            // Área = (base * altura) / 2
+            double area = (baseTriangulo * alturaTriangulo) / 2;
+            Console.WriteLine($"El área del triángulo es: {area}");
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Triángulo con base {baseTriangulo} y altura {alturaTriangulo}");
+        }
+    }
+}
