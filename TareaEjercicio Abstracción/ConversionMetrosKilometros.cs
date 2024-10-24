78ad03c348ef10b21d08bd6969b81ebb89a9fda0
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-24 17:44:13 -0600

    Create ConversionMetrosKilometros.cs

---
+++ TareaEjercicio Abstracción/ConversionMetrosKilometros.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace TareaEjercicio_Abstracción
+{
+    public class ConversionMetrosKilometros : FormulaMatematica
+    {
+        private double metros;
+
+        public ConversionMetrosKilometros(double metros)
+        {
+            this.metros = metros;
+        }
+
+        public override void Calcular()
+        {
+            // 1 kilómetro = 1000 metros
+            double kilometros = metros / 1000;
+            Console.WriteLine($"{metros} metros son {kilometros} kilómetros.");
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Conversión de {metros} metros a kilómetros");
+        }
+    }
+}
