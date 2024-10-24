ba09eb4ea5c9871484f86532aa211def04cac1bd
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-24 17:45:38 -0600

    Create VolumenCilindro.cs

---
+++ TareaEjercicio Abstracción/VolumenCilindro.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace TareaEjercicio_Abstracción
+{
+    public class VolumenCilindro : FormulaMatematica
+    {
+        private double radio;
+        private double altura;
+
+        public VolumenCilindro(double radio, double altura)
+        {
+            this.radio = radio;
+            this.altura = altura;
+        }
+
+        public override void Calcular()
+        {
+            // Volumen = π * r^2 * h
+            double volumen = Math.PI * Math.Pow(radio, 2) * altura;
+            Console.WriteLine($"El volumen del cilindro es: {volumen}");
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Cilindro con radio {radio} y altura {altura}");
+        }
+    }
+}
